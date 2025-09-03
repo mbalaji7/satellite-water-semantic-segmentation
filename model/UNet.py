ab8de4d7217f0ac9e94e950ede9d54e357f33ad3
mbalaji7 <mbalaji7@umd.edu>
2025-09-03 14:31:31 -0400

    Working UNet Architecture. Create Model Library for UNet

---
+++ model/UNet.py
@@ -0,0 +1,128 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Encoding convolution layers
+        # Down Layer 1
+        self.down_11 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.down_12 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down Layer 2
+        self.down_21 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.down_22 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.max_pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down Layer 3
+        self.down_31 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.down_32 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.max_pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down Layer 4
+        self.down_41 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.down_42 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.max_pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Final Handoff Layer
+        self.down_51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.down_52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1)
+
+        # Decoding Layers
+        # Up Layer 1
+        self.up_1t = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.up_11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
+        self.up_12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+
+        # Up Layer 2
+        self.up_2t = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.up_21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.up_22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+
+        # Up Layer 3
+        self.up_3t = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.up_31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.up_32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+
+        # Up Layer 4
+        self.up_4t = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.up_41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.up_42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+
+        # Final output layer
+        self.output_layer = nn.Conv2d(64, 2, kernel_size=1)
+
+    def forward(self, x):
+        # Encoding Layers
+        # Down Layer 1
+        d_11 = F.relu(self.down_11(x))
+        d_12 = F.relu(self.down_12(d_11))
+        d_1 = self.max_pool1(d_12)
+        print("Down Layer 1: ", d_1.shape)
+
+        # Down Layer 2
+        d_21 = F.relu(self.down_21(d_1))
+        d_22 = F.relu(self.down_22(d_21))
+        d_2 = self.max_pool2(d_22)
+        print("Down Layer 2: ", d_2.shape)
+
+        # Down Layer 3
+        d_31 = F.relu(self.down_31(d_2))
+        d_32 = F.relu(self.down_32(d_31))
+        d_3 = self.max_pool3(d_32)
+        print("Down Layer 3: ", d_3.shape)
+
+        # Down Layer 4
+        d_41 = F.relu(self.down_41(d_3))
+        d_42 = F.relu(self.down_42(d_41))
+        d_4 = self.max_pool4(d_42)
+        print("Down Layer 3: ", d_4.shape)
+
+        # Down Layer 5 - Final Handoff Layer
+        d_51 = F.relu(self.down_51(d_4))
+        d_52 = F.relu(self.down_52(d_51))
+        print("Down Layer 5: ", d_52.shape)
+        print("\n\n")
+
+        # Encoding Layers
+        # Up Layer 1
+        u_1t = self.up_1t(d_52)
+        print("Up ConvT Layer 1: ", u_1t.shape)
+        u_1t_42 = torch.cat([u_1t, d_42], dim=1)
+        u_11 = F.relu(self.up_11(u_1t_42))
+        u_12 = F.relu(self.up_12(u_11))
+        print("Up Layer 1: ", u_12.shape)
+
+        # Up Layer 2
+        u_2t = self.up_2t(u_12)
+
+        print("Up ConvT Layer 2: ", u_2t.shape)
+        u_2t_32 = torch.cat([u_2t, d_32], dim=1)
+        u_21 = F.relu(self.up_21(u_2t_32))
+        u_22 = F.relu(self.up_22(u_21))
+        print("Up Layer 2: ", u_22.shape)
+
+        # Up Layer 3
+        u_3t = self.up_3t(u_22)
+        print("Up ConvT Layer 3: ", u_3t.shape)
+        u_3t_22 = torch.cat([u_3t, d_22], dim=1)
+        u_31 = F.relu(self.up_31(u_3t_22))
+        u_32 = F.relu(self.up_32(u_31))
+        print("Up Layer 3: ", u_32.shape)
+
+        # Up Layer 4
+        u_4t = self.up_4t(u_32)
+        print("Up ConvT Layer 4: ", u_4t.shape)
+        u_4t_12 = torch.cat([u_4t, d_12], dim=1)
+        u_41 = F.relu(self.up_41(u_4t_12))
+        u_42 = F.relu(self.up_42(u_41))
+        print("Up Layer 4: ", u_42.shape)
+
+        # Final Output Layer
+        output = self.output_layer(u_42)
+
+        return output
--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -23,138 +23,73 @@ import mlflow.pytorch
 # Matplotlib Libraries for plotting images
 import matplotlib.pyplot as plt
 
+# Importing class from other pages
+from model.UNet import UNet
+
+"""
+All these functions inside the class is required by the DataLoader for its operations
+"""
+
 
 class WaterBodiesDataset(Dataset):
-    def __init__(self, image_dir, masks_dir, transform=None):
+    """
+    The input is the directory path for the images, directory path for the masks, and by default the transform parameter is None, but you can pass through a transform argument
+    """
+
+    def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir
-        self.masks_dir = masks_dir
+        self.mask_dir = mask_dir
         self.transform = transform
         self.images = os.listdir(image_dir)
 
-    # Two definitions related to DataLoader
+    # Return the length of the image directory
     def __len__(self):
         return len(self.images)
 
+    # Get images for that particular idx
     def __getitem__(self, idx):
-        # Because both of them contains the same image names, using self.images
+        # Path for the particular image
         image_path = os.path.join(self.image_dir, self.images[idx])
-        mask_path = os.path.join(self.masks_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx])
 
-        # Converting the image to RGB values
+        # Converting the image to RGB Values
         image = Image.open(image_path).convert("RGB")
 
-        # L is called Luminance. It is essentially converting the pixel values between 0, 255
+        # Getting the luminance values for black and white images
         mask = Image.open(mask_path).convert("L")
 
         if self.transform:
-            image = self.transform(image)
-            mask = self.transform(mask)
+            image, mask = self.transform(image), self.transform(mask)
 
         return image, mask
 
 
-class UNetConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, padding=1),
-            nn.ReLU(inplace=True),
-        )
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class UNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        # Creating the down layers
-        self.down1 = UNetConv(3, 64)
-        self.down2 = UNetConv(64, 128)
-        self.down3 = UNetConv(128, 256)
-        self.down4 = UNetConv(256, 512)
-
-        # Creating the Up Layers. Cascading to the down layers
-        self.up4 = UNetConv(512 + 256, 256)
-        self.up3 = UNetConv(256 + 128, 128)
-        self.up2 = UNetConv(128 + 64, 64)
-
-        # Final Layer
-        self.final_layer = nn.Conv2d(64, 1, 1)
-
-    def forward(self, x):
-        # Going through the first conv layer
-        down_1 = self.down1(x)
-        down_1 = F.max_pool2d(down_1, (2, 2))
-
-        # Second down layer
-        down_2 = self.down2(down_1)
-        down_2 = F.max_pool2d(down_2, (2, 2))
-
-        # Third down layer
-        down_3 = self.down3(down_2)
-        down_3 = F.max_pool2d(down_3, (2, 2))
-
-        # Fourth final down layer
-        down_4 = self.down4(down_3)
-        down_4 = F.max_pool2d(down_4, (2, 2))
-
-        """
-            Starting with the up layer
-            You get the data from the previous layer
-            Do an interpolation operation on the previous layer and assign it to the current up layer
-            Do a torch.cat with the current up layer and previous down layer
-            And do a up
-        """
-        up_4 = F.interpolate(
-            down_4, scale_factor=2, mode="bilinear", align_corners=True
-        )
-        up_4_concat = torch.cat([up_4, down_3], dim=1)
-        up_4 = self.up4(up_4_concat)
-
-        # Going to the next up layer
-        up_3 = F.interpolate(up_4, scale_factor=2, mode="bilinear", align_corners=True)
-        up_3_concat = torch.cat([up_3, down_2], dim=1)
-        up_3 = self.up3(up_3_concat)
-
-        # Going to the next up layer
-        up_2 = F.interpolate(up_3, scale_factor=2, mode="bilinear", align_corners=True)
-        up_2_concat = torch.cat([up_2, down_1], dim=1)
-        up_2 = self.up2(up_2_concat)
-
-        # Final Layer
-        up_1 = self.final_layer(up_2)
-
-        return up_1
-
-
 def main():
+    # Use CUDA if available else CPU
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    # Resize the image to strict 256, 256 without preserving the aspect ratio
     transform = transforms.Compose(
-        [transforms.Resize((256, 256)), transforms.ToTensor()]
+        [transforms.Resize((512, 512)), transforms.ToTensor()]
     )
 
+    # Directories for Mask and Images
     image_dir = os.path.join(Path().absolute(), "water_bodies_dataset", "Images")
-    masks_dir = os.path.join(Path().absolute(), "water_bodies_dataset", "Masks")
+    mask_dir = os.path.join(Path().absolute(), "water_bodies_dataset", "Masks")
 
-    # Creating an instance of the dataset that returns object for the dataloader
-    dataset = WaterBodiesDataset(image_dir, masks_dir, transform)
+    # Creating an instance of the WaterBodiesDataset class
+    dataset = WaterBodiesDataset(image_dir, mask_dir, transform)
 
-    # Dataloader object
+    # Creating an instance of dataloader
     dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
-    # Initializing the network
-    net = UNet().to(device)
+    model = UNet().to(device)
 
-    x = torch.randn(1, 3, 2000, 2000)
+    img_value = torch.randn(1, 3, 512, 512)
 
-    y = net(x.to(device))
+    output = model(img_value.to(device))
 
-    print(y.shape)
+    print(output.shape)
 
 
 if __name__ == "__main__":
